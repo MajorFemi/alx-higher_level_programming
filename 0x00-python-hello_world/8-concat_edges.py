e5aaacee46ca2b5eaedb00d853afbb2c7bdf9bf7
MajorFemi <harlaohorluwafemi@gmail.com>
2022-11-30 18:03:28 +0000

    latest

--- 0x00-python-hello_world/8-concat_edges.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-string = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-string = string[39:66] + string[112:118] + string[0:6]
-print(string)
