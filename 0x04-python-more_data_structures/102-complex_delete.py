f184e43f8f320217833b668429ce9cf23fe39347
MajorFemi <harlaohorluwafemi@gmail.com>
2022-12-07 22:27:50 +0000

    latest fix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,11 +2,11 @@
 
 
 def complex_delete(a_dictionary, value):
-    """
-    Delete keys with a specific value in a dictionary
-    """
-    if a_dictionary is not None and type(a_dictionary) is dict:
-        to_delete = {k: v for k, v in a_dictionary.items() if v == value}
-        for key, val in to_delete.items():
-            del a_dictionary[key]
-    return (a_dictionary.copy())
+    """Delete keys with a specific value in a dictionary."""
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
+
+    return (a_dictionary)
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,5 +5,5 @@ def print_sorted_dictionary(a_dictionary):
     """
     prints a dictionary by ordered keys. keys assumed to be strings
     """
-    [print(f'{key}: {a_dictionary}')
+    [print("{}: {}".format(key, a_dictionary[key]))
         for key in sorted(a_dictionary)]
