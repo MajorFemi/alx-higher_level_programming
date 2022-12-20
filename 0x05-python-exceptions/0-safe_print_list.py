4a0ddbe10e4f3e6c5bffc15f060220e7bd075763
MajorFemi <harlaohorluwafemi@gmail.com>
2022-12-20 00:24:48 +0000

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             total += 1
-        except:
-            continue
+        except IndexError:
+            break
     print("")
     return (total)
