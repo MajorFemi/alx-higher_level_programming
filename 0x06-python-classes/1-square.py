8e0f867e70a3c7aa0236d8cf285f89d47734b1b8
MajorFemi <harlaohorluwafemi@gmail.com>
2022-12-21 15:21:15 +0000

    Class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size):
+        """Intialize a new square.
+        Args:
+            size(int): Size of the new square
+
+        """
+        self.__size = size
